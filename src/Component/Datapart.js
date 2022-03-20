4e582c627d1b5a23f2856d129188d8c081b33437
mohan2002 <mohanprasaath269@gmail.com>
2022-03-20 13:52:37 +0530

    filteroptions and status completed

--- src/Component/Datapart.js
+++ src/Component/Datapart.js
@@ -1,14 +1,126 @@
 import React from 'react'
 import "./Styles/Datapart.css"
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import { useState } from 'react';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import DesktopDatePicker from '@mui/lab/DesktopDatePicker';
+import TextField from '@mui/material/TextField';
+import {CgFileDocument} from "react-icons/cg"
+import {AiOutlineLoading3Quarters} from "react-icons/ai"
+import {GrCompliance} from "react-icons/gr"
+import {IoIosArrowForward} from "react-icons/io"
+
+
 
 function Datapart() {
+  const [name, setName] = React.useState("David White");
+  const[proj,setProj] = useState("");
+  const [value, setValue] = React.useState(new Date());
+  const filestyle = {color:'blue',fontSize:"1.3rem"}
+  const loadstyle = {color:'orange',fontSize:"1.3rem"}
+  const completestyle = {color:'green',fontSize:"1.3rem"}
+  const arrow = {color:'black',fontSize:"1.2rem",fontWeight:"600"};
+
+  const handleName = (event) => {
+    setName(event.target.value);
+  };
+
   return (
     <div className='datapart-container'>
       <h1 className='schedule'>Schedule</h1>
 
       <div className='datapart-hero'>
+        <section className='input-section'>
+          <div className='datapart-filters'>
+              <div>
+                <FormControl variant="standard" sx={{ m: 1, minWidth: 220 }}>
+                  <Select
+                    labelId="demo-simple-select-standard-label"
+                    id="demo-simple-select-standard"
+                    value={name}
+                    onChange={handleName}
+                  >
+                    <MenuItem value={"David White"}>David White</MenuItem>
+                    <MenuItem value={"Tim"}>Tim</MenuItem>
+                    <MenuItem value={"Abi"}>Abi</MenuItem>
+                  </Select>
+                </FormControl>
+              </div>
 
-      </div>
+              <div className='filterbyproj'>
+                <FormControl variant="standard" sx={{ m: 1, minWidth: 220 }}>
+                <InputLabel id="demo-simple-select-standard-label">Filter by Project</InputLabel>
+                  <Select
+                    labelId="demo-simple-select-standard-label"
+                    id="demo-simple-select-standard"
+                    value={proj}
+                    onChange={handleName}
+                  >
+                    <MenuItem value={"Project 1"}>Project 1</MenuItem>
+                    <MenuItem value={"Project 2"}>Project 2</MenuItem>
+                    <MenuItem value={"Project 3"}>Project 3</MenuItem>
+                  </Select>
+                </FormControl>
+              </div>
+              <div className='datepicker'>
+                <LocalizationProvider dateAdapter={AdapterDateFns}>
+                  <DesktopDatePicker
+                    inputFormat="dd/MM/yyyy"
+                    value={value}
+                    onChange={(newValue) => {
+                      setValue(newValue);
+                    }}
+                    renderInput={(params) => <TextField {...params} variant="standard"/>}
+                  />
+                </LocalizationProvider>
+              </div>
+          </div>
+
+
+          <div className="filter-buttons">
+            <button className='filter-btn'>APPLY FILTER</button>
+            <button className='filter-btn reset'>RESET</button>
+          </div>
+        </section>
+        
+
+        <section className='section-status'>
+          <div className='status'>
+            <div>
+              <CgFileDocument style={filestyle}/>
+              <p>To Do</p>
+            </div>
+            <div>
+                <IoIosArrowForward style={arrow}/>
+            </div>
+          </div>
+          
+          <div className='status'>
+              <div>
+                <AiOutlineLoading3Quarters style={loadstyle}/>
+                <p>In Progress</p>
+              </div>
+              <div>
+                  <IoIosArrowForward style={arrow}/>
+              </div>
+          </div>
+          
+          <div className='status'>
+              <div>
+                <GrCompliance style={completestyle}/>
+                <p>Completed</p>
+              </div>
+              <div>
+                  <IoIosArrowForward style={arrow}/>
+              </div>
+          </div>
+        </section>
+        
+      </div>     
     </div>
   )
 }
